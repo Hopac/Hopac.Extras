b0196eaad3c2389f50c75f5848870d8be7d4c0ab
Vasily Kirichenko <vaskir@gmail.com>
2015-01-17 19:48:32 +0300

    move to FAKE

---
+++ src/Hopac.Extras/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Hopac.Extras")>]
+[<assembly: AssemblyProductAttribute("Hopac.Extras")>]
+[<assembly: AssemblyDescriptionAttribute("The Hopac.Extras project contains useful abstractions implemented with Hopac.")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1.0"
---
+++ src/Hopac.Extras/Scripts/load-references.fsx
@@ -0,0 +1,7 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#r @"System.dll"
+#r @"System.Core.dll"
+#r @"System.Numerics.dll"
+#r @"..\..\..\lib\Hopac\Hopac.dll"
+#r @"..\..\..\lib\Hopac\Hopac.Core.dll"
+#r @"..\..\..\lib\Hopac\Hopac.Platform.dll"
