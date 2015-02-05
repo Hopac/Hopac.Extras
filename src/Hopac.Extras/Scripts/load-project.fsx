74769b741d314815c092a88b12ce2b77870381d2
Vasily Kirichenko <vaskir@gmail.com>
2015-02-05 23:08:10 +0300

    fix compilation

--- src/Hopac.Extras/Scripts/load-project.fsx
+++ src/Hopac.Extras/Scripts/load-project.fsx
@@ -1,6 +1,7 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #load @"load-references.fsx"
 #load @"..\Utils.fs"
+      @"..\Control.fs"
       @"..\ObjectPool.fs"
       @"..\ParallelExecutor.fs"
       @"..\IO.fs"
