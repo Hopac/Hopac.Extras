75f775b6084c8ff45255c7f5ff6e2015697d79fc
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2015-02-06 11:57:32 +0300

    add missing Test attributes
    cleanup

--- tests/Hopac.Extras.Tests/JobChoiceTests.fs
+++ tests/Hopac.Extras.Tests/JobChoiceTests.fs
@@ -7,26 +7,34 @@ open Hopac.Extras
 open FsCheck
 open FsCheck.Prop
 open System.Threading.Tasks
+open NUnit.Framework
 
 module Choice =
   let bind x2yC x = match x with Ok x -> x2yC x | Fail e -> Fail e
 
+let private check = Check.VerboseThrowOnFailure
+
+[<Test>]
 let ``result``() = Check.VerboseThrowOnFailure (fun x -> run (JobChoice.result x |>> fun r -> r = Ok x))
 
+[<Test>]
 let ``bind``() =
-  Check.VerboseThrowOnFailure (fun (x: Choice<_,_>) (x2yJ: _ -> Choice<_,_>) ->
+  check (fun (x: Choice<_,_>) (x2yJ: _ -> Choice<_,_>) ->
     run (JobChoice.bind (Job.result x) (x2yJ >> Job.result)) = Choice.bind x2yJ x)
 
+[<Test>]
 let ``bindAsync``() =
-  Check.VerboseThrowOnFailure (fun (x: Choice<int,_>) (Function.F(_, x2yJ: _ -> Choice<_,_>)) ->
+  check (fun (x: Choice<int,_>) (Function.F(_, x2yJ: _ -> Choice<_,_>)) ->
     run (JobChoice.bindAsync (async.Return x) (x2yJ >> Job.result)) = Choice.bind x2yJ x)
 
+[<Test>]
 let ``bindTask``() =
-  Check.VerboseThrowOnFailure (fun (x: Choice<int,_>) (Function.F(_, x2yJ: _ -> Choice<_,_>)) ->
+  check (fun (x: Choice<int,_>) (Function.F(_, x2yJ: _ -> Choice<_,_>)) ->
     run (JobChoice.bindTask (Task.FromResult x) (x2yJ >> Job.result)) = Choice.bind x2yJ x)
 
+[<Test>]
 let ``bindVoidTask``() =
-  Check.VerboseThrowOnFailure (fun (Function.F(_, x2yJ: _ -> Choice<_,_>)) ->
+  check (fun (Function.F(_, x2yJ: _ -> Choice<_,_>)) ->
     run (JobChoice.bindVoidTask (Task.Factory.StartNew(fun() -> ())) (x2yJ >> Job.result)) = x2yJ ())
 
   
