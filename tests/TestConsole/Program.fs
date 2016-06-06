f44e52d05b0b12dcae5de0832f0e242e39854fc3
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2016-06-06 14:56:42 +0300

    add ProcessRunner tests

---
+++ tests/TestConsole/Program.fs
@@ -0,0 +1,12 @@
+﻿open System.Threading
+open System
+
+[<EntryPoint>]
+let main = function
+    | [|exitAfterSeconds; exitCode|] ->
+        let exitAfterSeconds = int exitAfterSeconds
+        let exitCode = int exitCode
+        Console.WriteLine (sprintf "Will be exit with code = %d after %d ms timeout..." exitCode exitAfterSeconds)
+        Thread.Sleep (TimeSpan.FromSeconds (float exitAfterSeconds))
+        exitCode
+    | args -> failwithf "Wrong arguments: %A, expected <exit after seconds> <exit code>" args
