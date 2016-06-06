01c07a487c3082a3d94a4dd3dbae661dd1f00dc8
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2016-06-06 14:05:18 +0300

    update Hopac to version 0.2.1

--- paket-files/fsharp/FAKE/modules/Octokit/Octokit.fsx
+++ paket-files/fsharp/FAKE/modules/Octokit/Octokit.fsx
@@ -1,5 +1,7 @@
+#nowarn "211"
 #I __SOURCE_DIRECTORY__
 #I @"../../../../../packages/Octokit/lib/net45"
+#I @"../../packages/Octokit/lib/net45"
 #I @"../../../../../../packages/build/Octokit/lib/net45"
 #r "System.Net.Http"
 #r "Octokit.dll"
@@ -91,6 +93,26 @@ let createClientWithToken token =
         return github
     }
 
+let createGHEClient url user password =
+    async {
+        let credentials = Credentials(user, password)
+        let httpClient = new HttpClientWithTimeout(TimeSpan.FromMinutes 20.)
+        let connection = new Connection(new ProductHeaderValue("FAKE"), new Uri(url), new InMemoryCredentialStore(credentials), httpClient, new SimpleJsonSerializer())
+        let github = new GitHubClient(connection)
+        github.Credentials <- credentials
+        return github
+    }
+
+let createGHEClientWithToken url token =
+    async {
+        let credentials = Credentials(token)
+        let httpClient = new HttpClientWithTimeout(TimeSpan.FromMinutes 20.)
+        let connection = new Connection(new ProductHeaderValue("FAKE"), new Uri(url), new InMemoryCredentialStore(credentials), httpClient, new SimpleJsonSerializer())
+        let github = new GitHubClient(connection)
+        github.Credentials <- credentials
+        return github
+    }
+
 let private makeRelease draft owner project version prerelease (notes:seq<string>) (client : Async<GitHubClient>) =
     retryWithArg 5 client <| fun client' -> async {
         let data = new NewRelease(version)
@@ -98,7 +120,7 @@ let private makeRelease draft owner project version prerelease (notes:seq<string
         data.Body <- String.Join(Environment.NewLine, notes)
         data.Draft <- draft
         data.Prerelease <- prerelease
-        let! draft = Async.AwaitTask <| client'.Release.Create(owner, project, data)
+        let! draft = Async.AwaitTask <| client'.Repository.Release.Create(owner, project, data)
         let draftWord = if data.Draft then " draft" else ""
         printfn "Created%s release id %d" draftWord draft.Id
         return {
@@ -116,7 +138,7 @@ let uploadFile fileName (draft : Async<Draft>) =
         let fi = FileInfo(fileName)
         let archiveContents = File.OpenRead(fi.FullName)
         let assetUpload = new ReleaseAssetUpload(fi.Name,"application/octet-stream",archiveContents,Nullable<TimeSpan>())
-        let! asset = Async.AwaitTask <| draft'.Client.Release.UploadAsset(draft'.DraftRelease, assetUpload)
+        let! asset = Async.AwaitTask <| draft'.Client.Repository.Release.UploadAsset(draft'.DraftRelease, assetUpload)
         printfn "Uploaded %s" asset.Name
         return draft'
     }
@@ -132,6 +154,64 @@ let releaseDraft (draft : Async<Draft>) =
     retryWithArg 5 draft <| fun draft' -> async {
         let update = draft'.DraftRelease.ToUpdate()
         update.Draft <- Nullable<bool>(false)
-        let! released = Async.AwaitTask <| draft'.Client.Release.Edit(draft'.Owner, draft'.Project, draft'.DraftRelease.Id, update)
+        let! released = Async.AwaitTask <| draft'.Client.Repository.Release.Edit(draft'.Owner, draft'.Project, draft'.DraftRelease.Id, update)
         printfn "Released %d on github" released.Id
     }
+
+let getLastRelease owner project (client : Async<GitHubClient>) =
+    retryWithArg 5 client <| fun client' -> async {
+        let! draft = Async.AwaitTask <| client'.Repository.Release.GetLatest(owner, project)
+
+        printfn "Latest release id: %d" draft.Id
+        printfn "Latest release tag: %s" draft.TagName
+        printfn "Latest release assets: %d" (Seq.length draft.Assets)
+
+        return {
+            Client = client'
+            Owner = owner
+            Project = project
+            DraftRelease = draft }
+    }
+
+let getReleaseByTag owner project tag (client : Async<GitHubClient>) =
+    retryWithArg 5 client <| fun client' -> async {
+        let! drafts = Async.AwaitTask <| client'.Repository.Release.GetAll(owner, project)
+        let matches = drafts |> Seq.filter (fun (r: Release) -> r.TagName = tag)
+
+        if Seq.isEmpty matches then
+            failwithf "Unable to locate tag %s" tag
+
+        let draft = matches |> Seq.head
+
+        printfn "Release id: %d" draft.Id
+        printfn "Release tag: %s" draft.TagName
+        printfn "Release assets: %d" (Seq.length draft.Assets)
+
+        return {
+            Client = client'
+            Owner = owner
+            Project = project
+            DraftRelease = draft }
+    }
+
+let downloadAsset id destination (draft : Async<Draft>) =
+    retryWithArg 5 draft <| fun draft' -> async {
+        let! asset = Async.AwaitTask <| draft'.Client.Repository.Release.GetAsset(draft'.Owner,draft'.Project,id)
+        let! resp = Async.AwaitTask <| draft'.Client.Connection.Get(new Uri(asset.Url), new System.Collections.Generic.Dictionary<string,string>(),"application/octet-stream")
+
+        let bytes = resp.HttpResponse.Body :?> byte[]
+        let filename = Path.Combine(destination, asset.Name)
+
+        File.WriteAllBytes(filename, bytes)
+
+        printfn "Downloaded %s" filename
+    }
+
+let downloadAssets destination (draft : Async<Draft>) = async {
+    let! draft' = draft
+    let draftW = async { return draft' }
+
+    let! _ = Async.Parallel [for f in draft'.DraftRelease.Assets -> downloadAsset f.Id destination draftW ]
+
+    ()
+}
