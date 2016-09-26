856f7a429e2d292100daf21fd246019a40df289c
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2016-09-26 16:20:04 +0300

    Bump version to 0.3.1

--- src/Hopac.Extras/AssemblyInfo.fs
+++ src/Hopac.Extras/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Hopac.Extras")>]
 [<assembly: AssemblyProductAttribute("Hopac.Extras")>]
 [<assembly: AssemblyDescriptionAttribute("The Hopac.Extras project contains useful abstractions implemented with Hopac.")>]
-[<assembly: AssemblyVersionAttribute("0.3.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.3.0")>]
+[<assembly: AssemblyVersionAttribute("0.3.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.3.0"
-    let [<Literal>] InformationalVersion = "0.3.0"
+    let [<Literal>] Version = "0.3.1"
+    let [<Literal>] InformationalVersion = "0.3.1"
